6fb5876e991658fcb49a29980e1680682f31f6c6
borii1203 <1minji@naver.com>
2025-05-27 17:10:41 +0900

    완료

--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
-$(function(){
-                $('.header .nav_wrap .gnb > li').on('mouseenter', function () {
-                $(this).find('.subMenu_wrap').addClass('open')
-            }).on('mouseleave', function () {
-                $(this).find('.subMenu_wrap').removeClass('open')
-            });//2차 메뉴 열림
+$(function () {
+    $('.header .nav_wrap .gnb > li').on('mouseenter', function () {
+        $(this).find('.subMenu_wrap').addClass('open')
+    }).on('mouseleave', function () {
+        $(this).find('.subMenu_wrap').removeClass('open')
+    });//2차 메뉴 열림
 });//script end
--- js/main.js
+++ js/main.js
@@ -1,4 +1,57 @@
 $(function () {
     $('.header_wrap').load('common/header.html')
     $('.footer').load('common/footer.html')
+
+
+    var swiper = new Swiper(".bestSwiper", {
+        slidesPerView: 1.6,
+        spaceBetween: 40,
+        breakpoints: {
+            550: {
+                slidesPerView: 2.5,
+                spaceBetween: 20,
+            },
+            800: {
+                slidesPerView: 3,
+                spaceBetween: 35,
+            },
+            1024: {
+                slidesPerView: 3.5,
+                spaceBetween: 40,
+            },
+            1200: {
+                slidesPerView: 4.4,
+                spaceBetween: 40,
+            },
+            1400: {
+                slidesPerView: 5,
+                spaceBetween: 35,
+            },
+        },
+
+    });
+
+    var swiper = new Swiper(".itemSwiper", {
+        slidesPerView: 2.1,
+        spaceBetween: 40,
+        breakpoints: {
+            550: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+            },
+            768: {
+                slidesPerView: 4,
+                spaceBetween: 40,
+            },
+            1400: {
+                slidesPerView: 3,
+                spaceBetween: 40,
+            },
+        },
+
+        scrollbar: {
+            el: ".swiper-scrollbar",
+            hide: false,
+        },
+    });
 });//scirpt end
