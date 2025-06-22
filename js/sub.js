e1410fb067d62c3a4e4e04a820b93653308673b0
borii1203 <1minji@naver.com>
2025-06-22 09:09:57 +0900

    20250622

--- js/sub.js
+++ js/sub.js
@@ -1,4 +1,21 @@
 $(function () {
     $('.header_wrap').load('../common/header.html')
     $('.footer').load('../common/footer.html')
+
+    var swiper = new Swiper(".subSwiper", {
+        spaceBetween: 10,
+        slidesPerView: 4,
+        freeMode: true,
+        watchSlidesProgress: true,
+    });
+    var swiper2 = new Swiper(".mainSwiper", {
+        spaceBetween: 10,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        thumbs: {
+            swiper: swiper,
+        },
+    });
 });//scirpt end
